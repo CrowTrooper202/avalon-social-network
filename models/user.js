38b39ba7fa814c9fac2d7dadd130acfc0ec216c3
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-11 23:02:35 -0400

    build user model and started thought model

--- models/user.js
+++ models/user.js
@@ -1,2 +1,27 @@
-const mongoose = require('mongoose');
+const {schema, model} = require('mongoose');
+const thoughtSchema = require('./Though');
 
+
+const studentSchema = new schema(
+    {
+        username:{
+            type:String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email:{
+            type: String,
+            validate: [validator, `/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/`],
+            required: true,
+            unique:true,
+
+        },
+        thoughts: [thoughtSchema],
+        friends: [studentSchema],
+    }
+)
+
+const Student = model('student', studentSchema);
+
+module.exports = Student;
