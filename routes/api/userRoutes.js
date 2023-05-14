7a5bd3c1945110e7bcf8ba446ace575268bbd871
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-14 16:33:17 -0400

    finished API user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,14 @@
 const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createNewUser,
+    updateUser,
+    DeleteUser,
+} =require('../../controllers/userController');
+
+router.route('/').get(getUsers).post(createNewUser).get(getSingleUser).put(updateUser).delete(DeleteUser);
+
+router.route('/:userId').post(newFriend);
+
+router.route('/:friendId').delete(removeFriend)
