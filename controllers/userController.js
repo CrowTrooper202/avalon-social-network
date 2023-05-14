2409885d3a84d91a5239a0b9bfaf2183c9c98c2f
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-14 16:21:01 -0400

    wronte user contoller code

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,5 +12,39 @@ module.exports ={
         !course ?  res.status(404).json({ message: 'No course with that ID' })
         : res.json(course) )
         .catch((err)=> res.status(500).json(err))
+        .populate('users')
+        .select('-__v')
+        .then((user)=>
+        !course ?  res.status(404).json({ message: 'No course with that ID' })
+        : res.json(course) )
+        .catch((err)=> res.status(500).json(err))
     },
+    createNewUser(req, res){
+        User.create(req.body)
+        .then((user)=> res.Json(user))
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+          });
+    },
+    updateUser(req, res){
+        User.findOneAndUpdate(
+            {_id: req.params.UserId},
+            {$set: req.body},
+            {runValidators: true, new: true}
+        )
+        .then((user)=>
+        !course
+          ? res.status(404).json({ message: 'No course with this id!' })
+          : res.json(course))
+        .catch((err) => res.status(500).json(err));  
+    },
+    DeleteUser(req, res){
+        User.findOneAndDelete({_id: req.params.userId})
+        .then((user)=>
+        !user ?res.json({message: 'no user by that ID'})
+        :Thought.deleteMany({_id: {$in:user.thoughts}}))
+        .then(()=> res.json({mesage: 'user and thoughts removed'}))
+        .catch((error)=>res.status(500).json(err))
+    }
 }
